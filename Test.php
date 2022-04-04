acd020fb55325007e3580d11d322d6bcc1638020
Victor <victor.ledda.vic@gmail.com>
2022-04-04 11:01:45 +0200

    Final Commit Exo

--- Test.php
+++ Test.php
@@ -2,6 +2,25 @@
 
 use PHPUnit\Framework\TestCase;
 
+class ConvertToromain{ 
+
+    function ConvertNumber($number)
+    {
+        $result = '';
+        $symbols = [1000 => 'M', 900 => 'CM', 100 => 'C', '90' => 'XC', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 3 => 'III', 2 => 'II',1 => 'I'];
+    
+        foreach ($symbols as $value => $symbol) {
+            while ($number >= $value) {
+                $result .= $symbol;
+                $number -= $value;
+            }
+        }
+        return $result;
+    }
+
+
+}
+
 class Test extends TestCase
 {
 
@@ -9,6 +28,16 @@ class Test extends TestCase
     {
         $this->assertEquals('I', ConvertToromain(1));
         $this->assertEquals('II', ConvertToromain(2));
+    }
+
+    public function test3IsConvertedInIV()
+    {
+        $this->assertEquals('III', ConvertToromain(3));
+    }
+
+    public function test4IsConvertedInIV()
+    {
+        $this->assertEquals('IV', ConvertToromain(4));
     }
 
 }
