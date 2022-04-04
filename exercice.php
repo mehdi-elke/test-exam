b64e1f33b2af4daf8958a2733a14e28330fcb405
Victor <victor.ledda.vic@gmail.com>
2022-04-04 10:56:50 +0200

    Sixiéme Commit Exo

--- exercice.php
+++ exercice.php
@@ -2,19 +2,19 @@
 <?php
 class ConvertToromain{ 
 
-function convert($int)
-{
+    function ConvertNumber($number)
+    {
+        $result = '';
+        $symbols = [1000 => 'M', 900 => 'CM', 100 => 'C', '90' => 'XC', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 3 => 'III', 2 => 'II',1 => 'I'];
     
-        if ($int == 0) {
-            return '';
+        foreach ($symbols as $value => $symbol) {
+            while ($number >= $value) {
+                $result .= $symbol;
+                $number -= $value;
+            }
         }
-        $symbols = array(1 => I);
-
-        $romain = '';
-      
-      return $romain;
-    
-}
+        return $result;
+    }
 
 
 }
