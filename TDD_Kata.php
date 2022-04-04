13aefd01dfdcdd2d261b3353ad7b37939c4fbf6f
Ramlito <85298882+Ramlito@users.noreply.github.com>
2022-04-04 10:53:30 +0200

    commit #3

--- TDD_Kata.php
+++ TDD_Kata.php
@@ -1,11 +1,18 @@
 <?php
+
+use PHPUnit\Framework\TestCase;
+
 function intToRomanString($number){
     $res = '';
     $symbolesToInt = [
         1000 => 'M',
         900 => 'CM',
         500 => 'D',
+        400 => 'CD',
         100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
         10 => 'X',
         9 => 'IX',
         5 => 'V',
@@ -21,10 +28,28 @@ function intToRomanString($number){
     }
     return $res;
 }
-class TestIntToRoman extends \PHPUnit\Framework\TestCase {
+class TDD_Kata extends TestCase {
     public function testNumbersFrom1To3AreConvertedWithIs()
     {
         $this->assertEquals('I', intToRomanString(1));
         $this->assertEquals('II', intToRomanString(2));
+    }
+
+    public function test1999IsConvertedInMCMXCIX()
+    {
+        $this->assertEquals('M' . 'CM' . 'XC' . 'IX', intToRomanString(1999));
+    }
+    public function test692IsConvertedInDCXCII()
+    {
+        $this->assertEquals('D' . 'C' . 'XC' . 'II', intToRomanString(692));
+    }
+
+    public function test544IsConvertedInDXLIV()
+    {
+        $this->assertEquals('D' . 'XL' . 'IV', intToRomanString(544));
+    }
+    public function test431IsConvertedInCDXXXI() // Test les chiffres avec 400, avec 10, avec 1
+    {
+        $this->assertEquals('CD' . 'XXX' . 'I', intToRomanString(431));
     }
 }
